7d0988122168fb9a97549877c353223c7065cd2e
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-01-04 12:21:53 +0900

    Time: 2 ms (94.01%), Space: 50.1 MB (99.79%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,36 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    // 입력된 문자열에서 알파벳(a-z, A-Z)과 숫자(0-9)를 제외한 모든 문자를 제거하고,
+    // 남은 문자열을 소문자로 변환한다.
+    let str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
     // 글자 시작(start)과 끝(end) 포인터를 설정
     let start = 0;
-    let end = s.length - 1;
+    let end = str.length - 1;
 
     // 시작 포인터와 끝 포인터가 서로 만나거나 교차하기 전까지 반복
     while(start < end) {
-        // 시작 포인터가 알파벳/숫자가 아닌 경우
-        while(!isAlphaNumeric(s[start])) {
-            start ++;
-        }
-
-        // 끝 포인터가 알파벳/숫자가 아닌 경우
-        while(!isAlphaNumeric(s[end])) {
+        // 시작 포인터와 끝 포인터가 가리키는 문자가 동일한지 확인
+        if(str[start] === str[end]) {
+            start++;
             end--;
-        }
-
-        // 소문자로 변환 후 비교
-        if(s[start].toLowerCase() !== s[end].toLowerCase()) {
+        } else {
             return false;
         }
-
-        // 포인터 이동
-        start++;
-        end--;
     }
     return true;
-
 };
-
-// 알파벳/숫자인지 확인하는 함수
-function isAlphaNumeric(char) {
-    return /^[a-zA-Z0-9]$/.test(char);
-}
