4a278b8c372a922546e376327f09780baf430df0
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-01-07 23:21:25 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,17 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    for(let i=0;i<progresses.length;i++){
+        progresses[i]=Math.ceil((100-progresses[i])/speeds[i]);
+        
+        if(i!=0&&progresses[i-1]>progresses[i]) {
+            progresses[i]=progresses[i-1];
+        }
+    }
+
+    let i=0;
+    while(progresses.length) {
+        answer.push([...progresses].filter(v=>v==progresses[i]).length)
+        progresses=progresses.filter(v=>v!==progresses[i])
+    }
+    return answer;
+}
