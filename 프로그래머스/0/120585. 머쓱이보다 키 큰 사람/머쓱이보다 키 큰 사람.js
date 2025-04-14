c6d392b6ad13fcf7934575cc33b47e2be0b2a889
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-04-14 12:18:51 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,22 @@
 function solution(array, height) {
-    var answer = 0;
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] > height) {
-            answer++;
-        }
-    }
-    return answer;
+    // 1
+    // var answer = 0;
+    // for(let i = 0; i < array.length; i++) {
+    //     if(array[i] > height) {
+    //         answer++;
+    //     }
+    // }
+    // return answer;
+    
+    // 2
+    // let count = 0;
+    // for (i of array) {
+    //     if (i > height) {
+    //         count += 1
+    //     }
+    // }
+    // return count
+    
+    // 3
+    return array.filter(v => v > height).length;
 }
