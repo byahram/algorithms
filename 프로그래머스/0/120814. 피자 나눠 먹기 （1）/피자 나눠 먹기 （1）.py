53a0ce578a941ea173d83772bf76e0d0a6cfd056
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-04-14 11:30:11 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,6 +1,12 @@
+import math
+
 def solution(n):
-    if n // 7 == n / 7 :
-        return n // 7
-    else :
-        return n // 7 + 1
-    return answer
+    # 1
+    # if n // 7 == n / 7 :
+    #     return n // 7
+    # else :
+    #     return n // 7 + 1
+    # return answer
+    
+    # 2
+    return math.ceil(n / 7)
