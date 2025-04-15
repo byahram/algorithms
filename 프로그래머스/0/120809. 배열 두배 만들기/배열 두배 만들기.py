5444472baaad6ca82b15988a5f421f0243074e3c
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-04-15 16:17:04 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.19 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    # 1
+    # answer = []
+    # for i in numbers :
+    #     answer.append(i * 2)
+    # return answer
+    
+    # 2
+    return list(map(lambda x:x*2, numbers))
