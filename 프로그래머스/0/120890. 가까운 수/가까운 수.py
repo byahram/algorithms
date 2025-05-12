26745c09ea72ef4897faa3a7192bb2af3f371447
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-05-12 17:27:23 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,9 +1,13 @@
 def solution(array, n):
-    array = sorted(array)
-    diff = float("inf")
-    nearest = 0
-    for i in array :
-        if abs(n - i) < diff :
-            diff = n - i
-            nearest = i
-    return nearest
+    # 1.
+    # array = sorted(array)
+    # diff = float("inf")
+    # nearest = 0
+    # for i in array :
+    #     if abs(n - i) < diff :
+    #         diff = n - i
+    #         nearest = i
+    # return nearest
+    
+    # 2. 
+    return sorted(array, key = lambda x : (abs(x - n), x - n))[0]
