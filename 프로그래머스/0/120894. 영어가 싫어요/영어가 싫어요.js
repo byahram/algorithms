e7ea10ad445cac9300c5be325dfd5c279432891f
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-05-13 13:52:49 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,22 @@
+function solution(numbers) {
+    // 1. 
+    // return +numbers.replaceAll("one", "1").replaceAll("two", "2").replaceAll("three", "3").replaceAll("four", "4").replaceAll("five", "5").replaceAll("six", "6").replaceAll("seven", "7").replaceAll("eight", "8").replaceAll("nine", "9").replaceAll("zero", "0");
+    
+    // 2. 정규표현식
+    let obj = {
+        one: 1,
+        two: 2, 
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9,
+        zero: 0
+    }
+    
+    return +numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v]
+    })
+}
