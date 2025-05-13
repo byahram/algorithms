53dbb5f6299764ca71dcab4b16d695d912d7f9e6
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-05-13 14:15:44 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,3 +1,8 @@
 def solution(emergency):
     응급순서 = sorted(emergency, reverse=True)
-    return list(map(lambda x : 응급순서.index(x) + 1, emergency))
+    
+    # 1.
+    # return list(map(lambda x : 응급순서.index(x) + 1, emergency))
+    
+    # 2.
+    return [응급순서.index(i) + 1 for i in emergency]
