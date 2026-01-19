9d2637774d3d709829eefb05c0f0730b8f4b43ae
Ahram Kim <45866137+byahram@users.noreply.github.com>
2026-01-19 23:18:12 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/README.md
+++ 프로그래머스/0/120829. 각도기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 각도기 - 120829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 0.00 ms
+메모리: 9.32 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 13일 23:39:27
+2026년 01월 19일 23:18:09
 
 ### 문제 설명
 
--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,10 +1,9 @@
 def solution(angle):
-    if 0 < angle and angle < 90 :
-        answer = 1
-    elif angle == 90 :
-        answer = 2
-    elif angle > 90 and angle < 180 :
-        answer = 3
-    elif angle == 180 :
-        answer = 4
-    return answer
+    if angle < 90:
+        return 1
+    elif angle == 90:
+        return 2
+    elif angle < 180:
+        return 3
+    else:
+        return 4
