853c59f2eec86673f310dbc3cc403893291f3c3e
Ahram Kim <45866137+byahram@users.noreply.github.com>
2026-01-23 15:24:38 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,8 @@
 def solution(my_string):
-    answer = ''
-    for ch in my_string:
-        if ch not in ["a", "e", "i", "o", "u"]:
-            answer += ch
-    return answer
+    # answer = ''
+    # for ch in my_string:
+    #     if ch not in ["a", "e", "i", "o", "u"]:
+    #         answer += ch
+    # return answer
+
+    return "".join([i for i in my_string if not(i in "aeiou")])
