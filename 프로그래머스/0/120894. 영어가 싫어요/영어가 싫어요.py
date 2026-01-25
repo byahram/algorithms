49af901ecce76033c5d258cbdb84a4b90d18b961
Ahram Kim <45866137+byahram@users.noreply.github.com>
2026-01-25 19:09:29 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,25 +1,8 @@
 import re
 
 def solution(numbers):
-    # 1.
-    # return int(numbers.replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "9").replace("zero", "0"))
-    
-    # 2.
-    s = ''
-    obj = {
-        'one': '1',
-        'two': '2', 
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9',
-        'zero': '0'
-    }
-    
-    for i in re.findall(r'(zero|one|two|three|four|five|six|seven|eight|nine)', numbers) :
-        s += obj[i]
-    return int(s)
+    r = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    for k in r.keys():
+        numbers = numbers.replace(k, r[k])
+    return int(numbers)
     
