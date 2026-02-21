671fe64e89a31f5f425b2170f38f9638405ebf1d
Ahram Kim <45866137+byahram@users.noreply.github.com>
2026-02-21 22:10:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.00 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,29 @@
-from collections import Counter
+# from collections import Counter
 
 def solution(participant, completion):
-    diff = Counter(participant) - Counter(completion)
-    return next(iter(diff))
+    # 3. hash dictionary 사용
+    hash_dict = {}
+
+    for p in participant:
+        hash_dict[p] = hash_dict.get(p, 0) + 1
+
+    for c in completion:
+        hash_dict[c] -= 1
+
+    for k in hash_dict:
+        if hash_dict[k] > 0:
+            return k
+    
+    # 2. 
+    # participant.sort()
+    # completion.sort()
+    # 
+    # for p, c in zip(participant, completion):
+    #     if p != c:
+    #         return p
+    # 
+    # return participant[-1]
+    
+    # 1. Counter 사용
+    # diff = Counter(participant) - Counter(completion)
+    # return next(iter(diff))
