07b9ca4e0ee8c420206d164b87627fc8f288e0bb
Ahram Kim <45866137+byahram@users.noreply.github.com>
2026-02-23 12:47:44 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    # 1. 의상 종류별로 개수 세기
+    closet = {}
+    for _, kind in clothes:
+        closet[kind] = closet.get(kind, 0) + 1
+    
+    # 2. (종류별 개수 + 1)을 모두 곱하기
+    answer = 1
+    for count in closet.values():
+        answer *= (count + 1)
+        
+    # 3. 최소 하나는 입어야 하므로 '전체 안 입음' 경우인 1을 뺌
+    return answer - 1
