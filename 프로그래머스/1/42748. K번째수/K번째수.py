f2fd9729de7aa2a08b9e556ec61e9abcb3760425
Ahram Kim <45866137+byahram@users.noreply.github.com>
2026-08-11 23:06:28 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,8 @@
 def solution(array, commands):
     answer = []
-    for c in commands:
-        temp = array[c[0]-1:c[1]]
+    for command in commands:
+        i, j, k = command
+        temp = array[i-1:j]
         temp.sort()
-        answer.append(temp[c[2]-1])
+        answer.append(temp[k-1])
     return answer
